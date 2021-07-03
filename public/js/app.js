0215155487babf9e033b9da7a737a854e8cba988
Ms.MONTHARA KIATRUANGCHAI <63130500224@st.sit.kmutt.ac.th>
2021-07-03 18:14:53 +0700

    remove unneccesary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("Client side javascript file is loaded!");
-
 const weatherForm = document.querySelector('form');
 const inputElem = document.querySelector('input');
 const message1Elem = document.getElementById('message-1');
